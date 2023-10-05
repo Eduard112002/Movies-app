ad5b0c735609d18bcb925e04ab2cef64a5bd1622
Eduard112002 <123071598+Eduard112002@users.noreply.github.com>
2023-10-05 19:21:32 +0300

    Финальное состояние

--- src/server/services.js
+++ src/server/services.js
@@ -1,15 +1,15 @@
 import { Component } from 'react';
 
 export default class Services extends Component {
+  apiKey = 'c59bd581055dff7ef5c5bc3d5d83b40d';
+
   async getListService(current, title) {
     const titleDefault = title ? title : 'return';
     const url = `https://api.themoviedb.org/3/search/movie?query=${titleDefault}&include_adult=false&language=en-US&page=`;
-    const res = await fetch(`${url}${current}`, {
+    const res = await fetch(`${url}${current}&api_key=${this.apiKey}`, {
       method: 'GET',
       headers: {
         accept: 'application/json',
-        Authorization:
-          'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNTliZDU4MTA1NWRmZjdlZjVjNWJjM2Q1ZDgzYjQwZCIsInN1YiI6IjY0Zjk5NWEyNGNjYzUwMTg2OGRhYWY4NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.rHjOe79qNqjRa4Bh2siGFrYZS7v35gTI0Gx_o3dRebM',
       },
     });
 
@@ -36,51 +36,41 @@ export default class Services extends Component {
     return await resID.json();
   }
 
-  id = null;
-
   async getId() {
     const res = await this.createGuestSession();
-    this.id = res.guest_session_id;
+    return res.guest_session_id;
   }
 
-  async getRatedMovies(current) {
-    const id = this.id;
+  async getRatedMovies(current, guestSessionId) {
     const res = await fetch(
-      `https://api.themoviedb.org/3/account/${id}/rated/movies?language=en-US&page=${current}&sort_by=created_at.asc`,
+      `https://api.themoviedb.org/3/guest_session/${guestSessionId}/rated/movies?language=en-US&page=${current}&api_key=${this.apiKey}`,
       {
         method: 'GET',
-        headers: {
-          accept: 'application/json',
-          Authorization:
-            'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNTliZDU4MTA1NWRmZjdlZjVjNWJjM2Q1ZDgzYjQwZCIsInN1YiI6IjY0Zjk5NWEyNGNjYzUwMTg2OGRhYWY4NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.rHjOe79qNqjRa4Bh2siGFrYZS7v35gTI0Gx_o3dRebM',
-        },
       }
     ).then((response) => response.json());
     return [res.results, res.total_pages];
   }
-
-  async addRating(page, id) {
-    await fetch(`https://api.themoviedb.org/3/movie/${id}/rating`, {
-      method: 'POST',
-      headers: {
-        accept: 'application/json',
-        'Content-Type': 'application/json;charset=utf-8',
-        Authorization:
-          'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNTliZDU4MTA1NWRmZjdlZjVjNWJjM2Q1ZDgzYjQwZCIsInN1YiI6IjY0Zjk5NWEyNGNjYzUwMTg2OGRhYWY4NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.rHjOe79qNqjRa4Bh2siGFrYZS7v35gTI0Gx_o3dRebM',
-      },
-      body: JSON.stringify({
-        value: page,
-      }),
-    });
-  }
+  addRating = (page, idFilm, guestSessionId) => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${idFilm}/rating?guest_session_id=${guestSessionId}&api_key=${this.apiKey}`,
+      {
+        method: 'POST',
+        headers: {
+          accept: 'application/json',
+          'Content-Type': 'application/json;charset=utf-8',
+        },
+        body: JSON.stringify({
+          value: page,
+        }),
+      }
+    );
+  };
 
   async getGenres() {
-    const res = await fetch('https://api.themoviedb.org/3/genre/movie/list?language=en', {
+    const res = await fetch(`https://api.themoviedb.org/3/genre/movie/list?language=en&api_key=${this.apiKey}`, {
       method: 'GET',
       headers: {
         accept: 'application/json',
-        Authorization:
-          'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNTliZDU4MTA1NWRmZjdlZjVjNWJjM2Q1ZDgzYjQwZCIsInN1YiI6IjY0Zjk5NWEyNGNjYzUwMTg2OGRhYWY4NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.rHjOe79qNqjRa4Bh2siGFrYZS7v35gTI0Gx_o3dRebM',
       },
     }).then((response) => response.json());
     return res.genres;
